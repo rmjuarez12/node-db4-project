1e0f135637f8215c5f98d20b7572cd5a19776873
rmjuarez12 <rmjuarez12@gmail.com>
2021-02-05 05:16:10 -0500

    Setup and configured migration file

--- data/migrations/20210205035618_recipes-schema.js
+++ data/migrations/20210205035618_recipes-schema.js
@@ -1,8 +1,49 @@
-
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+  return knex.schema
+    .createTable("recipes", (tbl) => {
+      tbl.increments();
+      tbl.string("recipe_name").unique().notNullable();
+    })
+    .createTable("ingredients", (tbl) => {
+      tbl.increments();
+      tbl.string("ingredient_name").unique().notNullable();
+    })
+    .createTable("preparationSteps", (tbl) => {
+      tbl.increments();
+      tbl
+        .integer("recipe_id")
+        .unsigned()
+        .notNullable()
+        .references("recipes.id")
+        .onUpdate("CASCADE")
+        .onDelete("CASCADE");
+      tbl.integer("step_num").unsigned().notNullable();
+      tbl.string("step_description", 1000);
+    })
+    .createTable("ingredientDetails", (tbl) => {
+      tbl
+        .integer("recipe_id")
+        .unsigned()
+        .notNullable()
+        .references("recipes.id")
+        .onUpdate("CASCADE")
+        .onDelete("CASCADE");
+      tbl
+        .integer("ingredient_id")
+        .unsigned()
+        .notNullable()
+        .references("ingredients.id")
+        .onUpdate("CASCADE")
+        .onDelete("CASCADE");
+      tbl.float("quantity").unsigned();
+      tbl.primary(["recipe_id", "ingredient_id"]);
+    });
 };
 
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+  return knex.schema
+    .dropTableIfExists("ingredientDetails")
+    .dropTableIfExists("preparationSteps")
+    .dropTableIfExists("ingredients")
+    .dropTableIfExists("recipes");
 };
